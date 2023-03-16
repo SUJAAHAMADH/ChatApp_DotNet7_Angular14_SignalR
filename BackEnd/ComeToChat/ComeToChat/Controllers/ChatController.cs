511684bb6cabaa5611010f4e103cc4813b4ed4cb
SUJAAHAMADH <sujasgk2@gmail.com>
2023-03-16 17:37:12 +0530

    initial-commit

---
+++ BackEnd/ComeToChat/ComeToChat/Controllers/ChatController.cs
@@ -0,0 +1,30 @@
+﻿using ComeToChat.Dtos;
+using ComeToChat.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComeToChat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        private readonly ChatService _chatService;
+
+        public ChatController(ChatService chatService)
+        {
+            _chatService = chatService; 
+        }
+
+        [HttpPost("register-user")]
+        public IActionResult RegisterUser(UserDto model)
+        {
+            if (_chatService.AddUserToList(model.Name))
+            {
+                return NoContent (); 
+            }
+
+            return BadRequest("This name is taken please choose another name");
+        }
+    }
+}
---
+++ BackEnd/ComeToChat/ComeToChat/Hubs/ChatHub.cs
@@ -0,0 +1,29 @@
+﻿using ComeToChat.Services;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace ComeToChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly ChatService _chatService; 
+
+        public ChatHub(ChatService chatService)
+        {
+            _chatService = chatService;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "Come2Chat");
+            await Clients.Caller.SendAsync("UserConnected");
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, "Come2Chat");
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
